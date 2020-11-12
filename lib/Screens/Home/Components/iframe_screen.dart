eded43f64d56bbb56c7505226b376facb46ea74a
Colin Hunwald <colin.hunwald@gmail.com>
2020-11-12 19:28:21 +0100

    Add second video

--- lib/Screens/Home/Components/iframe_screen.dart
+++ lib/Screens/Home/Components/iframe_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 import 'dart:ui' as ui;
 import 'package:etar_app/Screens/Home/Components/app_bar.dart';
+import 'package:etar_app/constant.dart';
 import 'package:flutter/material.dart';
 
 class IframeScreen extends StatefulWidget {
@@ -17,14 +18,20 @@ class _IframeScreenState extends State<IframeScreen> {
 
   final _textController = TextEditingController();
 
+  List<String> _videos = ['https://www.youtube.com/embed/bbPvddL2L7Q',
+    "https://www.youtube.com/embed/Bm1o_RgD3LA",
+  ];
+  var i = 0;
+
   @override
   void initState() {
     super.initState();
 
+
     _iframeElement.height = '500';
     _iframeElement.width = '500';
 
-     _iframeElement.src = 'https://www.youtube.com/embed/bbPvddL2L7Q';
+     _iframeElement.src = _videos[i];
     _iframeElement.style.border = 'none';
 
     // ignore: undefined_prefixed_name
@@ -37,6 +44,12 @@ class _IframeScreenState extends State<IframeScreen> {
       key: UniqueKey(),
       viewType: 'iframeElement',
     );
+  }
+
+  void _newVideo(int i) {
+    setState(() {
+      _iframeElement.src = _videos[i];
+    });
   }
 
   @override
@@ -53,10 +66,44 @@ class _IframeScreenState extends State<IframeScreen> {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.only(top: 80),
-              child: SizedBox(
-                height: _height,
-                width: _width,
-                child: _iframeWidget,
+              child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+
+                      TextButton.icon(
+                        style: TextButton.styleFrom(
+                          side: BorderSide(color: i == 0 ? crimson : Colors.blueGrey[900], width: 2),
+                        ),
+                        icon: Icon(Icons.play_arrow_outlined),
+                        label: Text('ETAR Intro'),
+                        onPressed: () {
+                          i = 0;
+                          return _newVideo(0);
+                        },
+                      ),
+
+                      TextButton.icon(
+                        style: TextButton.styleFrom(
+                          side: BorderSide(color: i == 1 ? crimson : Colors.blueGrey[900], width: 2),
+                        ),
+                        icon: Icon(Icons.play_arrow_outlined),
+                        label: Text('ETAR kód kérés'),
+                        onPressed: () {
+                          i = 1;
+                          return _newVideo(1);
+                        },
+                      ),
+
+                    ],
+                  ),
+                  SizedBox(
+                    height: _height,
+                    width: _width,
+                    child: _iframeWidget,
+                  ),
+                ],
               ),
             ),
           ),
