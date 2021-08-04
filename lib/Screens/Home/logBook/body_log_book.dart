79e6d488764a012076b22b240c1cdf7a2da1a3bd
Colin Hunwald <colin.hunwald@gmail.com>
2021-08-04 16:16:48 +0200

    logBook new color

--- lib/Screens/Home/logBook/body_log_book.dart
+++ lib/Screens/Home/logBook/body_log_book.dart
@@ -5,10 +5,31 @@ class BodyLogBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Container(
-
+        width: size.width,
+        height: size.height * 0.7,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 50, top: 80),
+              child: Column(
+                children: [
+                  Container(
+                    constraints: BoxConstraints(maxHeight: 100),
+                    child: Image.asset(
+                      "assets/images/logBookIcon.png",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
